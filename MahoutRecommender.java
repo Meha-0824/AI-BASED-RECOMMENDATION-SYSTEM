5d1af9d0b5237d1df418d5d2562e40a01125384e
MEHA JABEEN A M <mehaafrin09@gmail.com>
2025-12-15 20:34:10 +0530

    Add files via upload

---
+++ MahoutRecommender.java
@@ -0,0 +1,40 @@
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+import java.io.File;
+import java.util.List;
+
+public class MahoutRecommender {
+
+    public static void main(String[] args) throws Exception {
+
+        DataModel model = new FileDataModel(new File("data.csv"));
+
+        UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+
+        UserNeighborhood neighborhood =
+                new NearestNUserNeighborhood(2, similarity, model);
+
+        Recommender recommender =
+                new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+        List<RecommendedItem> recommendations =
+                recommender.recommend(1, 3);
+
+        for (RecommendedItem item : recommendations) {
+            System.out.println(
+                    "Recommended Item: " +
+                    item.getItemID() +
+                    " | Score: " +
+                    item.getValue()
+            );
+        }
+    }
+}
